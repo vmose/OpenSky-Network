75cae52fad02b429d3d773f908b564f59f3dba49
vmose <vicmose.vm@gmail.com>
2026-09-03 15:12:36 +0300

    init 1

--- ingestion/extract.py
+++ ingestion/extract.py
@@ -1,1 +1,17 @@
+import requests
 
+try:
+    response = requests.get(
+        "https://opensky-network.org/api/states/all"
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+
+        print(f"Timestamp: {data['time']}")
+        print(f"Number of aircraft: {len(data['states'])}")
+    else:
+        print(f"Error: Received status code {response.status_code}")
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
